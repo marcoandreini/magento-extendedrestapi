de179edd447e31b7a7ac27b14cd1a55a7e57920e
Marco Andreini <marco.andreini@gmail.com>
2013-05-20 10:57:07 +0200

    Initial working release.

--- app/code/community/Ikom/ExtendRestApi/Model/Api2/Category/Rest/Admin/V1.php
+++ app/code/community/Ikom/ExtendRestApi/Model/Api2/Category/Rest/Admin/V1.php
@@ -3,27 +3,27 @@
 /**
  * Override for Magento's Catalog REST API
  */
-class Ikom_ExtendRestApi_Model_Api2_Category_Rest_Admin_V1 extends Mage_Catalog_Model_Api2_Product_Rest {
+class Ikom_ExtendRestApi_Model_Api2_Category_Rest_Admin_V1 extends Mage_Api2_Model_Resource {
 
 	/**
 	 * Retrieve the category
 	 * @return category entity
 	 */
     protected function _retrieve() {
-    	Mage::log("categories retrieve called");
+    	// Mage::log("categories retrieve called");
     	$category_id = $this->getRequest()->getParam('id');
-        $category = Mage::getResourceModel('catalog/category')->load($category_id);
-        // $store = $this->_getStore();
-        // $category->setStoreId($store->getId());
+        $category = Mage::getModel('catalog/category')->load($category_id);
+        $category->setStoreId($this->_getStore()->getId());
 
         $data = array();
-    	$data['category_id'] = $category_id;
+    	$data['entity_id'] = $category_id;
     	$data['name'] = $category->getName();
     	$data['parent_id'] = $category->getParentId();
-    	$data['child_id'] = array(); // TODO: $subcats
-    	$data['active'] = $category->getIsActive() ? 1 : 0;
+    	$data['children'] = $category->getResource()->getChildren($category, false);
+    	$data['is_active'] = $category->getIsActive() ? 1 : 0;
     	$data['level'] = $category->getLevel();
     	$data['position'] = $category->getPosition();
+    	$data['url_key'] = $category->getUrlKey();
     	return $data;
     }
 
@@ -33,17 +33,14 @@ class Ikom_ExtendRestApi_Model_Api2_Category_Rest_Admin_V1 extends Mage_Catalog_
      * @return int
      */
      protected function _retrieveCollection() {
-     	Mage::log("categories retrieve collection called");
-        $collection = Mage::getResourceModel('catalog/category')->getCollection();
-        $store = $this->_getStore();
-        $collection->setStoreId($store->getId());
-        $collection->addAttributeToSelect(array_keys(
-                $this->getAvailableAttributes($this->getUserType(), Mage_Api2_Model_Resource::OPERATION_ATTRIBUTE_READ)
-        ));
-        $this->_applyCategoryFilter($collection);
+        $model = Mage::getModel('catalog/category');
+        $model->setStoreId($this->_getStore()->getId());
+        $collection = $model->getCollection()
+         	->addAttributeToSelect(array_keys(
+                 $this->getAvailableAttributes($this->getUserType(), Mage_Api2_Model_Resource::OPERATION_ATTRIBUTE_READ)
+ 	        ));
         $this->_applyCollectionModifiers($collection);
-        $categories = $collection->load()->toArray();
-        return $categories['items'];
+        return $collection->load()->toArray();
     }
 }
 
